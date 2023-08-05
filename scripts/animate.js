8275849dae35b6d6881936e9b5fc741d05bb4a4e
Sheldon Codling <codlinsh@gmail.com>
2023-08-05 02:14:34 -0400

    Updated file structure to reduce critical path

--- scripts/animate.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", (event) => {
-    document.getElementById("outerHexagon").style.opacity = "1";
-    document.getElementById("outerHexagon").style.transform = "translateY(0px)";
-    document.getElementById("sideSect").style.opacity = "1";
-    document.getElementById("sideSect").style.transform = "translateX(0px)";
-})
